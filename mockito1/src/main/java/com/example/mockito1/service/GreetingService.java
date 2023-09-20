7565f4c5fed853c1f155468257b85316d53282eb
telugu srikanth <sri.tech81@gmail.com>
2023-09-20 16:26:55 +0530

    world

---
+++ mockito1/src/main/java/com/example/mockito1/service/GreetingService.java
@@ -0,0 +1,11 @@
+package com.example.mockito1.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GreetingService {
+
+	public String getGreeting() {
+        return "Hello SkSrikanth";
+    }
+}
