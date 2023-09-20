501d25a6c6bfaa77aab95f841a9bd0ba38abac24
telugu srikanth <sri.tech81@gmail.com>
2023-09-20 16:29:06 +0530

    warn

---
+++ mockito1/src/main/java/com/example/mockito1/Mockito1Application.java
@@ -0,0 +1,13 @@
+package com.example.mockito1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mockito1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mockito1Application.class, args);
+	}
+
+}
