07feb93fd906db797d53918bd8beaddf208aefd4
telugu srikanth <sri.tech81@gmail.com>
2023-09-20 16:25:54 +0530

    hello

---
+++ mockito1/src/main/java/com/example/mockito1/controller/GreetingController.java
@@ -0,0 +1,23 @@
+package com.example.mockito1.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.mockito1.service.GreetingService;
+
+@RestController
+public class GreetingController {
+
+	private final GreetingService greetingService;
+
+    @Autowired
+    public GreetingController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
+    @GetMapping("/greet")
+    public String greet() {
+        return greetingService.getGreeting();
+    }
+}
